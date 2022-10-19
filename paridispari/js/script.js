2a394836ff1c319d3c8f092e8a27703df9ae8f50
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-19 18:50:00 +0200

    [add] h6.innerText

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -6,7 +6,7 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 const userEvenOdd = document.querySelector('select');
-const numberInput = document.querySelector('input');
+const numberInput = document.querySelector('#usernumber');
 const btn = document.querySelector('button');
 
 
@@ -14,33 +14,48 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(){
   const numberUser = parseInt((numberInput.value)) ;
   const numberCpu = Math.ceil(Math.random() * 5);
+  const userChoice = userEvenOdd.value;
 
   console.log('numberUser', numberUser);
   console.log('numberCpu', numberCpu);
-  
+  console.log('userChoice', userChoice);
   
   const total = sommaNumber(numberUser, numberCpu);
 
-  sommaNumber(numberUser,numberCpu);
   console.log('questa è la somma', total);
 
-  paridispari(total);
-
-  
+  paridispari(total,userChoice,numberCpu);
  })
 
 
- function sommaNumber(n1,n2){
+ function sommaNumber(n1,n2) {
   const somma = n1 + n2;
 
   return somma;
  }
 
- function paridispari(number){
-  if(number % 2 === 0){
-    console.log('numero è pari');
+ function paridispari(number, userSelect, numberPc){
+  if(number % 2 === 0 && userSelect === "pari"){
+    document.querySelector('h6').innerText = `
+      Il numero del pc è ${numberPc}, la somma è ${number} = pari, hai vinto
+    `;
+    console.log('numero è pari, hai vinto');
 
+  }else if(number % 2 === 0 && userSelect === "dispari"){
+    document.querySelector('h6').innerText = `
+    Il numero del pc è ${numberPc}, la somma è ${number} = pari, hai perso
+  `;
+    console.log('numero è pari, hai perso');
+
+  }else if(!(number % 2 === 0) && userSelect === "dispari"){
+    document.querySelector('h6').innerText = `
+    Il numero del pc è ${numberPc}, la somma è ${number} = dispari, hai vinto
+  `;
+    console.log('Il numero è dispari, hai vinto');
   }else{
-    console.log('numero è dispari');
+    document.querySelector('h6').innerText = `
+    Il numero del pc è ${numberPc}, la somma è ${number} = dispari, hai perso
+  `;
+    console.log('Il numero è dispari, hai perso');
   }
  }
